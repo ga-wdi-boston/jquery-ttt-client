1cd38a8f98876a62db0920cbb7e3ffcbd9427af6
Antony Donovan <antony.donovan@generalassemb.ly>
2016-01-31 20:48:49 -0500

    Updates for evolving game-api
    Using sign-[up|in|out] and change-password for urls.

--- index.js
+++ index.js
@@ -1,60 +1,62 @@
 'use strict';
+let token = null;
+
 var tttapi = {
   gameWatcher: null,
-  ttt: 'http://ttt.wdibos.com',
+  ttt: 'http://localhost:3000',
 
-  ajax: function(config, cb) {
+  ajax: function ajax(config, cb) {
     $.ajax(config).done(function(data, textStatus, jqxhr) {
       cb(null, data);
     }).fail(function(jqxhr, status, error) {
-      cb({jqxher: jqxhr, status: status, error: error});
+      cb({ jqxher: jqxhr, status: status, error: error });
     });
   },
 
   register: function register(credentials, callback) {
     this.ajax({
-      method: 'POST',
       // url: 'http://httpbin.org/post',
-      url: this.ttt + '/users',
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(credentials),
-      dataType: 'json'
+      url: this.ttt + '/sign-up',
+      method: 'POST',
+      processData: false,
+      contentType: false,
+      data: credentials,
     }, callback);
   },
 
   login: function login(credentials, callback) {
     this.ajax({
-      method: 'POST',
       // url: 'http://httpbin.org/post',
-      url: this.ttt + '/login',
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(credentials),
-      dataType: 'json'
+      url: this.ttt + '/sign-in',
+      method: 'POST',
+      processData: false,
+      contentType: false,
+      data: credentials,
     }, callback);
   },
 
   //Authenticated api actions
-  listGames: function (token, callback) {
+  listGames: function listGames(token, callback) {
     this.ajax({
       method: 'GET',
       url: this.ttt + '/games',
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
-      dataType: 'json'
+      dataType: 'json',
     }, callback);
   },
 
-  createGame: function (token, callback) {
+  createGame: function (data, token, callback) {
     this.ajax({
       method: 'POST',
       url: this.ttt + '/games',
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify({}),
-      dataType: 'json',
+      processData: false,
+      contentType: false,
+      data,
     }, callback);
   },
 
@@ -63,110 +65,92 @@ var tttapi = {
       method: 'GET',
       url: this.ttt + '/games/' + id,
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
-      dataType: 'json'
     }, callback);
   },
 
-  joinGame: function (id, token, callback) {
+  joinGame: function (id, data, token, callback) {
     this.ajax({
       method: 'PATCH',
       url: this.ttt + '/games/' + id,
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify({}),
-      dataType: 'json'
+      processData: false,
+      contentType: false,
+      data,
     }, callback);
   },
 
-  markCell: function (id, data, token, callback) {
+  markCell: function(id, data, token, callback) {
     this.ajax({
       method: 'PATCH',
       url: this.ttt + '/games/' + id,
       headers: {
-        Authorization: 'Token token=' + token
+        Authorization: 'Token token=' + token,
       },
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify(data),
-      dataType: 'json'
+      processData: false,
+      contentType: false,
+      data,
     }, callback);
   },
 
   watchGame: function (id, token) {
     var url = this.ttt + '/games/' + id + '/watch';
     var auth = {
-      Authorization: 'Token token=' + token
+      Authorization: 'Token token=' + token,
     };
     this.gameWatcher = resourceWatcher(url, auth); //jshint ignore: line
     return this.gameWatcher;
-  }
+  },
 };
-
 
 //$(document).ready(...
 $(function() {
-  var form2object = function(form) {
-    var data = {};
-    $(form).children().each(function(index, element) {
-      var type = $(this).attr('type');
-      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
-        data[$(this).attr('name')] = $(this).val();
-      }
-    });
-    return data;
-  };
-  var wrap = function wrap(root, formData) {
-    var wrapper = {};
-    wrapper[root] = formData;
-    return wrapper;
-  };
-
-  var callback = function callback(error, data) {
+  const callback = function callback(error, data) {
     if (error) {
       console.error(error);
-      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      $('#result').val('status: ' + error.status + ', error: ' + error.error);
       return;
     }
+
     $('#result').val(JSON.stringify(data, null, 4));
   };
 
-  $('#register').on('submit', function(e) {
-    var credentials = wrap('credentials', form2object(this));
+  $('#sign-up').on('submit', function(e) {
+    let credentials = new FormData(e.target);
     tttapi.register(credentials, callback);
     e.preventDefault();
   });
 
-  $('#login').on('submit', function(e) {
-    var credentials = wrap('credentials', form2object(this));
+  $('#sign-in').on('submit', function(e) {
+    var credentials = new FormData(e.target);
     var cb = function cb(error, data) {
       if (error) {
         callback(error);
         return;
       }
+
       callback(null, data);
-      $('.token').val(data.user.token);
+      token = data.user.token;
     };
+
     e.preventDefault();
     tttapi.login(credentials, cb);
   });
 
   $('#list-games').on('submit', function(e) {
-    var token = $(this).children('[name="token"]').val();
     e.preventDefault();
     tttapi.listGames(token, callback);
   });
 
   $('#create-game').on('submit', function(e) {
-    var token = $(this).children('[name="token"]').val();
     e.preventDefault();
-    tttapi.createGame(token, callback);
+    tttapi.createGame(new FormData(), token, callback);
   });
 
   $('#show-game').on('submit', function(e) {
-    var token = $(this).children('[name="token"]').val();
     var id = $('#show-id').val();
     e.preventDefault();
     tttapi.showGame(id, token, callback);
@@ -187,25 +171,26 @@ $(function() {
     tttapi.markCell(id, data, token, callback);
   });
 
-  $('#watch-game').on('submit', function(e){
+  $('#watch-game').on('submit', function(e) {
     var token = $(this).children('[name="token"]').val();
     var id = $('#watch-id').val();
     e.preventDefault();
 
     var gameWatcher = tttapi.watchGame(id, token);
 
-    gameWatcher.on('change', function(data){
+    gameWatcher.on('change', function(data) {
       var parsedData = JSON.parse(data);
       if (data.timeout) { //not an error
         this.gameWatcher.close();
         return console.warn(data.timeout);
       }
+
       var gameData = parsedData.game;
       var cell = gameData.cell;
       $('#watch-index').val(cell.index);
       $('#watch-value').val(cell.value);
     });
-    gameWatcher.on('error', function(e){
+    gameWatcher.on('error', function(e) {
       console.error('an error has occured with the stream', e);
     });
   });
